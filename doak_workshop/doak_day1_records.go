2c3a9fead43361e4dbeaccf59737b4f28991c79c
foghorn <ushesh@users.noreply.github.com>
2015-02-01 18:07:21 -0800

    Finishing Doak's workshop's day 1 exercises

--- doak_workshop/doak_day1_records.go
+++ doak_workshop/doak_day1_records.go
@@ -4,7 +4,56 @@ import (
 	"fmt"
 )
 
+type Record struct {
+	First, Last string
+	ID int
+}
+
+type AllRecords struct {
+	Entries []Record
+	nextID int
+}
+
+// fmt.Println knows how to interpret the output of this String function which
+// returns a string and uses Sprintf
+func (r *Record) String() string{
+	return fmt.Sprintf("%d, %s, %s", r.ID, r.Last, r.First)
+}
+
+func (r *AllRecords) AddRecord(first, last string) {
+	n := Record{
+		First: first,
+		Last: last,
+		ID: r.nextID,
+	}
+	r.Entries = append(r.Entries, n)
+	r.nextID++
+}
+
 // Doak's workshop - day 1, exercises with records
 func main() {
+	// 1st exercise
+	a := Record{
+		ID: 1,
+		First: "John",
+		Last: "Doe",
+	}
+	fmt.Println(&a)
 
+	// 2nd exercise
+	var num int64
+	pointer := &num
+	num = 5
+	fmt.Println(num)
+	*pointer = 7
+	fmt.Println(num)
+
+	// 3rd exercise
+	records := &AllRecords{}
+	records.AddRecord("Flash", "Gordon")
+	records.AddRecord("Hawk", "Eye")
+	records.AddRecord("GI", "Joe")
+	for _, employee := range records.Entries {
+		fmt.Println(&employee)
+	}
 }
